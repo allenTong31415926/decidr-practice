eceeaf13f297d4eab2e84a8ad26877e8e1f31c15
allenTong31415926 <allen.tong31415926@gmail.com>
2025-01-26 14:35:56 +1100

    Add uniq index to locations and affiliations table

--- db/migrate/20250126000402_create_affiliations.rb
+++ db/migrate/20250126000402_create_affiliations.rb
@@ -1,9 +1,11 @@
 class CreateAffiliations < ActiveRecord::Migration[8.0]
   def change
     create_table :affiliations do |t|
-      t.string :name, null: false, unique: true
+      t.string :name, null: false
 
       t.timestamps
     end
+
+    add_index :affiliations, :name, unique: true
   end
 end
