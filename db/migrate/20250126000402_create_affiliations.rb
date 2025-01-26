edff779fa45604a441a72dd129797103a4924791
allenTong31415926 <allen.tong31415926@gmail.com>
2025-01-26 12:31:43 +1100

    Add uniq constrain to locations and affiliations tables

--- db/migrate/20250126000402_create_affiliations.rb
+++ db/migrate/20250126000402_create_affiliations.rb
@@ -1,7 +1,7 @@
 class CreateAffiliations < ActiveRecord::Migration[8.0]
   def change
     create_table :affiliations do |t|
-      t.string :name, null: false
+      t.string :name, null: false, unique: true
 
       t.timestamps
     end
